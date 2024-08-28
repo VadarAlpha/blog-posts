d27bf3410471bca505d68f45d86442004007255b
lbs-vadar <lbs@vadar.ai>
2024-08-28 11:11:50 -0500

    sorted the formatting

--- scripts/run_supply_chain.py
+++ scripts/run_supply_chain.py
@@ -3,11 +3,13 @@ import os
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 # Ensure the script is running from the correct directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(script_dir, '..'))
+project_root = os.path.abspath(os.path.join(script_dir, ".."))
 if project_root not in sys.path:
     sys.path.append(project_root)
 
@@ -15,9 +17,13 @@ from supplychain.simulation import build_supply_chain_from_config, simulate_supp
 from supplychain.visualization import visualize_supply_chain
 
 logging.info("Build supply chain from a configuration file")
-config_filepath = os.path.join(project_root, 'sim_config.json')
-combined_graph_filepath = os.path.join(project_root, 'combined_graph.graphml')  # Path to the GraphML file
-G, road_networks = build_supply_chain_from_config(config_filepath, combined_graph_filepath)
+config_filepath = os.path.join(project_root, "sim_config.json")
+combined_graph_filepath = os.path.join(
+    project_root, "combined_graph.graphml"
+)  # Path to the GraphML file
+G, road_networks = build_supply_chain_from_config(
+    config_filepath, combined_graph_filepath
+)
 
 logging.info("Simulate the supply chain")
 simulate_supply_chain(G)
